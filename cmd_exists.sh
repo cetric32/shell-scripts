39ba06811b94329feddca02b2ac42b9c5b1bebda
cetric <cetric32@gmail.com>
2019-03-18 17:56:45 +0300

    error correction

--- cmd_exists.sh
+++ cmd_exists.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-if [ -n $1 ]; then 
-{
+if [ -n $1 ]; then {
   if ! [ -x "$(command -v $1 )" ]; then
     echo "Error: $1 is not installed." >&2
     exit 1
-   else 
+  else 
     echo "$1 is already  installed."
   fi
 }
-else
-{
-echo "Error: No argument provided." >&2
-    exit 1
+else {
+	echo "Error: No argument provided." >&2
+	exit 1
 }
+fi
